d18f0c3fbed1895375733cc6fa00306387e4ec1b
Kevin Torres <kevintu236@gmail.com>
2025-06-02 21:22:48 -0600

    Solución de un error de la configuración de NativeWind
    - Había un error en el archivo de configuración de Tailwind que impedía que se mostraran los estilos correctamente.

--- tip_calculator/app/(tabs)/index.tsx
+++ tip_calculator/app/(tabs)/index.tsx
@@ -1,17 +1,16 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet, View, Text, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 import { useState } from 'react';
+import { verifyInstallation } from 'nativewind';
 
 export default function HomeScreen() {
   const [username, setUsername] = useState("");
+  verifyInstallation();
 
-  const ShowAlert = () => {
-    alert("Button pressed");
-  }
   return (
     <SafeAreaView style={styles.safe}>
-      <View>
-        <Text className='font-bold text-red-500'>Hello World!</Text>
+      <View className='flex-1 items-center justify-center'>
+        <Text className="text-red-500">Hello World!</Text>
       </View>
     </SafeAreaView>
   );
@@ -20,7 +19,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   safe: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     paddingTop: Platform.OS === "android" ? 25 : 20,
     paddingHorizontal: 20,
     paddingBottom: 20,
